cfdf04e3496180b3baf9881f7c1c68992d3fa47e
Thomas Girard <geek@thomasgirard.fr>
2015-04-29 08:17:15 +0200

    Creating the key handler

--- src/js/KeyHandler.js
+++ src/js/KeyHandler.js
@@ -1,4 +1,119 @@
-var KeyHandler = function()
-{
+/**
+ * @file Declaration of the key handler
+ * @author Thomas Girard <geek@thomasgirard.fr>
+ * @copyright 2015 (c) Thomas Girard
+ */
 
+/**
+ * KeyHandler
+ *
+ * Individual handling of keyboard keys
+ *
+ * @constructor
+ * @param {string} svgIdentifier - the element's dom id of the key.
+ * @param {int} keyCode - the code assigned to the key
+ */
+var KeyHandler = function(svgIdentifier, keyCode)
+{
+    // Set the keyCode value:
+    if ( ! isNaN(keyCode) ) {
+        this.code = parseInt(keyCode);
+    } else {
+        console.log("Invalid keyCode value (NaN)");
+    }
+
+    // Attach a svg element to the key:
+    this.elem = document.getElementById(svgIdentifier);
+}
+
+/**
+ * keyCode assigned to the object
+ * @memberof KeyHandler.prototype
+ */
+KeyHandler.prototype.code = -1;
+
+/**
+ * Svg element assigned to the key
+ * @memberof KeyHandler.prototype
+ */
+KeyHandler.prototype.elem = undefined;
+
+/**
+ * Map of characters assigned to the key
+ * @memberof KeyHandler.prototype
+ */
+KeyHandler.prototype.charList = undefined;
+
+/**
+ * Enable the key
+ * @memberof KeyHandler
+ */
+KeyHandler.prototype.on = function()
+{
+    console.log("You press the key: " + this.code);
+    return this;
+}
+
+/**
+ * Disable the key
+ * @memberof KeyHandler
+ */
+KeyHandler.prototype.off = function()
+{
+    return this;
+}
+
+/**
+ * Set a list of characters to the key
+ * @memberof KeyHandler
+ * @param {array} charList - list of characters associated to the key
+ * @returns {self}
+ */
+KeyHandler.prototype.setCharacters = function(charList)
+{
+    this.charList = charList.map( function(char) {
+        return ( isNaN(char) ) ? char.charCodeAt(0) : char;
+    });
+
+    return this;
+}
+
+/**
+ * Get the default character associated with the key
+ * @memberof KeyHandler
+ * @returns {int} charCode of the key
+ */
+KeyHandler.prototype.getChar = function()
+{
+    return this.charList[0];
+}
+
+/**
+ * Get the character associated with the key and [Shift]
+ * @memberof KeyHandler
+ * @returns {int} charCode of the key
+ */
+KeyHandler.prototype.getShiftChar = function()
+{
+    return this.charList[1];
+}
+
+/**
+ * Get the character associated with the key and [Alt Gr]
+ * @memberof KeyHandler
+ * @returns {int} charCode of the key
+ */
+KeyHandler.prototype.getAltChar = function()
+{
+    return this.charList[2];
+}
+
+/**
+ * Get the character associated with the key and [Shift] + [Alt Gr]
+ * @memberof KeyHandler
+ * @returns {int} charCode of the key
+ */
+KeyHandler.prototype.getShiftAltChar = function()
+{
+    return this.charList[3];
 }
